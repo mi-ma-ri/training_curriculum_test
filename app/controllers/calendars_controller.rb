4883fe0ab5751f317f4b574737d60c3b6db9e792
mi-ma-ri <michihiros93@gmail.com>
2022-03-17 14:15:54 +0900

    issue1-3の解消(2回目) getWeek→get_week

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,11 +2,9 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-<<<<<<< Updated upstream
-    get_Week
-=======
-    getWeek
->>>>>>> Stashed changes
+    
+    get_week
+
     @plan = Plan.new
   end
 
@@ -22,11 +20,8 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-<<<<<<< Updated upstream
-  def get_Week
-=======
-  def getWeek
->>>>>>> Stashed changes
+  def get_week
+
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
